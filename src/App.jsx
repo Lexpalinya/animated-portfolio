d3cc09531eb19e79e48d661db3341381c5be419d
Alex <137197995+Lexpalinya@users.noreply.github.com>
2024-01-14 00:44:37 +0700

    make show hero and parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 // import Test from "./Test";
-import "./app.scss"
+import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/parallax";
 
 const App = () => {
-  return <div>
-  
-    <section id="Homepage"><Navbar/></section>
-    <section >Parallax</section>
-    <section id="Services">Services</section>
-    <section>Parallax</section>
-    <section id="Portfolio">Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contact">Contact</section>
-    {/* <Test/> */}
-
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services"><Parallax type="Services"/></section>
+      <section>Services</section>
+      <section id="Portfolio"><Parallax type="Portfolio"/></section>
+      <section >Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+      {/* <Test/> */}
+    </div>
+  );
 };
 
 export default App;
